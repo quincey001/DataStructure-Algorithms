bbc82458017e09ac9f237795c6243787e58b9c1a
congqin yan <1727053412@qq.com>
2022-11-18 04:15:43 +0000

    add most common stack scenario

--- DS/src/main/java/data_structure/stack/Stack.java
+++ DS/src/main/java/data_structure/stack/Stack.java
@@ -51,5 +51,14 @@ public class Stack {
         stack.push(5);
         stack.pop();
         stack.printStack();
+        /**
+         * The most common uses of a stack are:
+         * 1 - To reverse a word - Put all the letters in a stack and pop them out.
+         *     Because of the LIFO order of stack, you will get the letters in reverse order.
+         * 2 - In compilers - Compilers use the stack to calculate the value of expressions like
+         *     2 + 4 / 5 * (7 - 9) by converting the expression to prefix or postfix form.
+         * 3 - In browsers - The back button in a browser saves all the URLs
+         *     you have visited previously in a stack.
+         */
     }
 }
