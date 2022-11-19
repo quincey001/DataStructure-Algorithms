79cc124ffab631f081d7cb2adc1c956a43396060
congqin yan <1727053412@qq.com>
2022-11-19 02:57:12 +0000

    reverse string using stack

--- DS/src/main/java/data_structure/stack/ReverseWord.java
+++ DS/src/main/java/data_structure/stack/ReverseWord.java
@@ -1,7 +1,20 @@
 package data_structure.stack;
 
 public class ReverseWord {
-    private String Reverse(String s){
-        return "";
+    private StringBuffer Reverse1(StringBuffer s){
+        Stack stack = new Stack(s.length());
+        for(int i = 0; i < s.length(); i++){
+            stack.push(s.charAt(i));
+        }
+        for(int i = 0; i < s.length(); i++){
+            char a = (char) stack.pop();
+            s.setCharAt(i, a);
+        }
+        return s;
+    }
+    public static void main(String[] args){
+        ReverseWord rw = new ReverseWord();
+        StringBuffer str = new StringBuffer("abcdefghijk");
+        System.out.println(rw.Reverse1(str));
     }
 }
