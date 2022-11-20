f47fdd300e40f8a5417f26ae5d6a8a6a701c6158
congqin yan <1727053412@qq.com>
2022-11-20 03:20:54 +0000

    leetcode 136

---
+++ DS/src/main/java/algorithms_array/leetcode136.java
@@ -0,0 +1,26 @@
+package algorithms_array;
+
+public class leetcode136 {
+    /**
+     * Given a non-empty array of integers nums, every element appears
+     * twice except for one. Find that single one.
+     *
+     * You must implement a solution with a linear runtime complexity and
+     * use only constant extra space.
+     * Input: nums = [2,2,1]
+     * Output: 1
+     */
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int i : nums){
+            res ^= i;
+            System.out.println(res);
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        int[] arr = new int[]{1,3,2,2,4,3,4};
+        leetcode136 l = new leetcode136();
+        l.singleNumber(arr);
+    }
+}
