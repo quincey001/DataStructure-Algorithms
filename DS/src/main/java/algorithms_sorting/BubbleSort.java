5fb3c21797a7f5af8b0641e0c74eb372f46d0317
congqin yan <1727053412@qq.com>
2022-11-23 21:02:43 +0000

    bubble sort algorithm

---
+++ DS/src/main/java/algorithms_sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package algorithms_sorting;
+
+public class BubbleSort {
+    public int[] bubbleSort(int[] arr){
+        int len = arr.length;
+
+        for(int i = 0; i < len - 1; i++){
+            for(int j = 0; j < len - 1 - i; j++){
+                if(arr[j] > arr[j+1]){
+                    int tem = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tem;
+                }
+            }
+        }
+        return arr;
+    }
+    void printArray(int[] arr){
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = new int[]{64,25,12,22,11};
+        BubbleSort b= new BubbleSort();
+        b.bubbleSort(arr);
+        b.printArray(arr);
+    }
+}
