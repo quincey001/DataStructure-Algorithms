b934ebd35482b1b193e6814e285935c3bdb56556
congqin yan <1727053412@qq.com>
2022-11-24 02:32:02 +0000

    quick sort algorithm

---
+++ DS/src/main/java/algorithms_sorting/QuickSort.java
@@ -0,0 +1,38 @@
+package algorithms_sorting;
+
+public class QuickSort {
+    static void swap(int[] arr, int i, int j){
+        int tem = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tem;
+    }
+    public static void quicksort(int[] array){
+        quicksort(array, 0, array.length - 1);
+    }
+    public static void quicksort(int[] array, int left, int right){
+        if(left >= right){
+            return;
+        }
+        int pivot = array[(left + right) / 2];
+        int index = partition(array, left, right, pivot);
+        quicksort(array, left, index - 1);
+        quicksort(array, index, right);
+    }
+    static int partition(int[] array, int left, int right, int pivot){
+        //pivot
+        while(left <= right){
+            while(array[left] < pivot){
+                left++;
+            }
+            while(array[right] > pivot){
+                right--;
+            }
+            if(left <= right){
+                swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+}
