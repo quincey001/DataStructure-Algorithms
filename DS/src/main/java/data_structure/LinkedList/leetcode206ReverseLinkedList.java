d910a9d933afbc5196446ec70fbd1fdd263684ac
congqin yan <1727053412@qq.com>
2022-12-08 01:34:44 +0000

    reverse linked list in java

--- DS/src/main/java/data_structure/LinkedList/leetcode206ReverseLinkedList.java
+++ DS/src/main/java/data_structure/LinkedList/leetcode206ReverseLinkedList.java
@@ -14,7 +14,29 @@ public class leetcode206ReverseLinkedList {
         current.next = new Node(data);
 
     }
-    public void reverse(){
-        
+    public String reverse(){
+        String val = "";
+        Node pre = null, cur = head;
+        while(cur != null){
+            Node nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        while(pre != null){
+            val += pre.value;
+            pre = pre.next;
+        }
+        return val;
     }
+    public static void main(String[] args){
+        leetcode206ReverseLinkedList l = new leetcode206ReverseLinkedList();
+        l.append(1);
+        l.append(2);
+        l.append(3);
+        l.append(4);
+        l.append(5);
+        System.out.println(l.reverse());
+    }
+
 }
