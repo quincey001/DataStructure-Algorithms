653112754714c53bbec8778e3a219bfb6c9ef1a1
congqin yan <1727053412@qq.com>
2022-12-08 02:18:51 +0000

    conditional statement problem in hackerrank

---
+++ DS/src/main/java/HackerRank/ConditionalStatements.java
@@ -0,0 +1,32 @@
+package HackerRank;
+import java.io.*; 
+public class ConditionalStatements {
+    /**
+     * Given an integer, , perform the following conditional actions:
+     *
+     * If  is odd, print Weird
+     * If  is even and in the inclusive range of  to , print Not Weird
+     * If  is even and in the inclusive range of  to , print Weird
+     * If  is even and greater than , print Not Weird
+     * Complete the stub code provided in your editor to print whether or not  is weird.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        if(N % 2 != 0){
+            System.out.println("Weird");
+        }else{
+            if(N >= 2 && N <= 5){
+                System.out.println("Not Weird");
+            }else if(N >= 6 && N <= 20){
+                System.out.println("Weird");
+            }else if(N > 20){
+                System.out.println("Not Weird");
+            }
+        }
+
+
+        bufferedReader.close();
+    }
+}
