cf5208377cd2f34eb46710aa5258c2d4ba957926
congqin yan <1727053412@qq.com>
2022-12-10 02:28:09 +0000

    hacker rank day 7

---
+++ DS/src/main/java/HackerRank/HankerrankDay7.java
@@ -0,0 +1,29 @@
+package HackerRank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+public class HankerrankDay7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+        String result = "";
+        for(int i = n - 1; i >= 0; i--){
+            result += Integer.toString(arr.get(i)) + " ";
+        }
+        System.out.print(result);
+        bufferedReader.close();
+    }
+}
