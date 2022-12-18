f958acd4c03d60e5c243b16bcc18c167c36a7f0d
congqin yan <1727053412@qq.com>
2022-12-18 01:08:25 +0000

    data structure of array

---
+++ DS/src/main/java/data_structure/Array/ArrayDemo.java
@@ -0,0 +1,18 @@
+package data_structure.Array;
+
+public class ArrayDemo {
+    /**
+     * what is an array?
+     * An array is the simplest data structure where a collection have a fixed size with the same type of
+     * data elements. and each element can be accessed by using the index number.
+     * Advantage:
+     * 1.random access
+     * 2. esay sorting and iteration
+     * 3. replacement of multiple variables
+     *
+     * Disadvantages:
+     * 1.fixed size
+     * 2. if capacity is bigger than the occupancy, rest of them are wasted
+     * 3. needs contiguous memory to get allocated.
+     */
+}
--- DS/src/main/java/data_structure/LinkedList/Node.java
+++ DS/src/main/java/data_structure/LinkedList/Node.java
@@ -4,11 +4,17 @@ import java.util.LinkedList;
 
 public class Node {
     /**
+     *
+     *
      * if you want to insert an element in the linkedlist it will take constant of the time
-     * if you want to delete an element in the linkedlist it
-     * just need constant time as well.
+     * if you want to delete an element in the linkedlist it just need constant time as well.
      * if you want to append an element to the linkedlist,
      * it will take linear of the time
+     *
+     * Advantages:
+     * 1. dynamic in size
+     * 2. no wastage as capacity and occupancy is always equal
+     * 3. efficient memory allocation
      *
      */
 
