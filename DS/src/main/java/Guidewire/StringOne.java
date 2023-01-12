98ee82468125c09cfbade9df2f3f4947b321c71a
congqin yan <1727053412@qq.com>
2023-01-12 11:25:56 +0000

    add a into string

---
+++ DS/src/main/java/Guidewire/StringOne.java
@@ -0,0 +1,29 @@
+package Guidewire;
+
+public class StringOne {
+    public String solve(String s){
+        StringBuilder sb = new StringBuilder(s);
+        int countA = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == 'a') {
+                countA++;
+                if (s.charAt(i+1) != 'a' && countA < 2) {
+                    sb.insert(i+1, 'a');
+                    countA ++;
+                }
+            } else {
+                if(s.charAt(i+1) != 'a' ){
+                    sb.insert(i+countA, "aa");
+                    countA += 2;
+                }else {
+                    countA = 0;
+                }
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args){
+        StringOne stringOne = new StringOne();
+        System.out.println(stringOne.solve("aababaahhh"));
+    }
+}
