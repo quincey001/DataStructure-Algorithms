c1f03adbba6497093ec72000fe888e655fa7c660
congqin yan <1727053412@qq.com>
2023-01-15 15:46:52 +0000

    insert and search method in trie

--- DS/src/main/java/data_structure/Trie/Trie.java
+++ DS/src/main/java/data_structure/Trie/Trie.java
@@ -8,6 +8,7 @@ public class Trie {
      * any node in trie can store non-repetitive multiple characters
      * every node stores link of the next character of the string
      * every node keeps track of end of string.
+     * insert and search method both take T(n) time complexity
      *
      * why we need trie?
      * to solve many standard problems in efficient way
@@ -25,6 +26,8 @@ public class Trie {
         TrieNode current = root;
         for(int i = 0; i < words.length(); i++){
             char ch = words.charAt(i);
+            // first thing is to check is the char in string weather exists in current root's children.
+            // if not, insert the char and a new node into current trie node
             TrieNode node = current.children.get(ch);
             if(node == null){
                 node = new TrieNode();
@@ -33,11 +36,34 @@ public class Trie {
             current = node;
         }
         current.isEndString = true;
-        System.out.println("insert " + words + "to trie successful!!");
+        System.out.println("insert " + words + " to trie successful!!");
     }
+
+    public boolean search(String words){
+        TrieNode current = root;
+        for(int i = 0; i < words.length(); i++){
+            char ch = words.charAt(i);
+            TrieNode node = current.children.get(ch);
+            if(node == null){
+                System.out.println( words + " string does not exist in this trie");
+                return false;
+            }
+            current = node;//move to children
+        }
+        if(current.isEndString == true){
+            System.out.println(words + " string exists in this trie");
+            return true;
+        }else{
+            System.out.println(words + " string does not exist in this trie, but it is a prefix of the Trie");
+        }
+        return current.isEndString;
+    }
+
+
 
     public static void main(String[] args){
         Trie newTrie = new Trie();
         newTrie.insert("hello");
+        newTrie.search("hel");
     }
 }
