aab6d0d59a9c5c313d534acf1ed9f9b651d725ab
congqin yan <1727053412@qq.com>
2023-01-16 21:21:13 +0000

    reverse words using string builder

--- DS/src/main/java/algorithms_string/Leetcode151ReverseWords.java
+++ DS/src/main/java/algorithms_string/Leetcode151ReverseWords.java
@@ -14,6 +14,7 @@ public class Leetcode151ReverseWords {
         }
         return newStr.trim();
     }
+    //12 ms
     public static String reverseWord(String s) {
         String[] stringArr = s.split(" +");//split the string by " " or multiple " "
         String newStr = "";
@@ -23,6 +24,18 @@ public class Leetcode151ReverseWords {
             index --;
         }
         return newStr.trim();
+    }
+    // 7 ms
+    public static String reverseWords2(String s) {
+        String[] words = s.split(" +");
+        StringBuilder sb = new StringBuilder();// string builder is faster
+        int index = words.length - 1;
+        while(index >= 0){
+            sb.append(words[index]);
+            sb.append(" ");
+            index--;
+        }
+        return sb.toString().trim();
     }
     public static void main(String[] args){
         System.out.println(reverseWords( "the sky is  blue"));
